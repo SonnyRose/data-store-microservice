46277ab4b549581e9885e51892875c9d2d73f747
SonnyRose <keeganwunder0@gmail.com>
2024-02-21 13:27:18 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/org/example/model/Summary.java

--- src/main/java/org/example/constants/MessageConstants.java
+++ src/main/java/org/example/constants/MessageConstants.java
@@ -1,9 +1,11 @@
 package org.example.constants;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MessageConstants {
+    //Constance for creating data from payload in DebeziumEventConsumerImpl.class
     public final static String ID_KEY = "id";
     public final static String SENSOR_ID_KEY = "sensor_id";
     public final static String MEASUREMENT_KEY = "measurement";
